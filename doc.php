a29ea09c1f4347e9cfbddfab97a3e15f16a8b1a9
vinayakkoliwad <vinayakkoliwad42@gmail.com>
2023-07-17 23:01:00 +0530

    add the doc

---
+++ doc.php
@@ -0,0 +1,11 @@
+https://www.positronx.io/create-simple-php-crud-rest-api-with-mysql-php-pdo/
+
+Navigate to the directory where you have located your PHP project.
+
+cd php-project-name
+
+Once you are in the project folder then run the following command to start the PHP app.
+
+php -S 127.0.0.1:8080
+
+Following output will be displayed on your terminal screen.
