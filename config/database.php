272da69b146da6ef08d8652ad4244ad3c55e2651
vinayakkoliwad <vinayakkoliwad42@gmail.com>
2023-07-17 21:24:31 +0530

    first commit for rest api for php

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php 
+    class Database {
+        private $host = "localhost:3307";
+        private $database_name = "phpapidb";
+        private $username = "vinayak";
+        private $password = "123456";
+        public $conn;
+        public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    }  
+?>
